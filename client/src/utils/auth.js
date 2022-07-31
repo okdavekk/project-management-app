11ecec81bd3a20fbd051403d0123e90c82bcc242
okdavekk <okdavekk@gmail.com>
2022-07-31 14:03:55 -0400

    Added auth, mutations, and queries starter code.

---
+++ client/src/utils/auth.js
@@ -0,0 +1,40 @@
+// TODO: Get this refactored and working if it doesnt already.
+
+import decode from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem('id_token');
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    // TODO: Is this right or should it be Project/:userId ?
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
---
+++ client/src/utils/queries.js
@@ -0,0 +1,45 @@
+// TODO: Got to look into these more
+
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_PROJECTS = gql`
+  query getProjects()
+
+`;
+
+export const QUERY_SINGLE_PROJECT = gql`
+  query getSingleProject()
+  }
+`;
+
+// TODO: What is this all about?
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      thoughts {
+        _id
+        thoughtText
+        thoughtAuthor
+        createdAt
+      }
+    }
+  }
+`;
