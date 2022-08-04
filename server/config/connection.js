b11f2c712136a6c8377fbbb341ef436580e01c93
James G <stackz@protonmail.com>
2022-08-04 10:36:00 -0400

    deploying again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/projectsDB',
+  process.env.MONGODB_URI || 'mongodb://localhost/projectsDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
